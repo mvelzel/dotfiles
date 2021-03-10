9541ded8e1c8df410c4d74287de79e3b6b706600
s1919377 <m.m.velzel@student.utwente.nl>
2021-03-10 09:58:40 +0100

    changed startwsl

--- wsl/start.ps1
+++ wsl/start.ps1
@@ -1,4 +1,4 @@
-(Get-Content "C:\PulseAudio\etc\pulse\default.pa") -replace "load-module module-native-protocol-tcp auth-ip-acl\b.*", "load-module module-native-protocol-tcp auth-ip-acl=$(Ubuntu run "ip addr show eth0 | grep -oP '(?<=inet\s)\d+(\.\d+){3}'")" | Set-Content "C:\PulseAudio\etc\pulse\default.pa"
+(Get-Content "C:\PulseAudio\etc\pulse\default.pa") -replace "load-module module-native-protocol-tcp auth-ip-acl\b.*", "load-module module-native-protocol-tcp auth-ip-acl=127.0.0.1" | Set-Content "C:\PulseAudio\etc\pulse\default.pa"
 wscript .\runHidden.vbs "C:\PulseAudio\bin\pulseaudio.exe"
 wscript .\runHidden.vbs ".\vim-caps.ahk"
 wscript .\runHidden.vbs "C:\Program Files\VcXsrv\vcxsrv.exe" :0 -screen 0 @1 -nodecoration -wgl -dpi 72 -xkbmodel pc105 -xkblayout us -keyhook -ac
